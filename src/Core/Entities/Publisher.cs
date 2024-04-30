69a41aa04f4b27cce7d4f36300da35b8f9d47374
Douglas <douglasvitor.dev@gmail.com>
2024-04-29 21:41:52 -0300

    Adding db seeder when it's staging environment

--- src/Core/Entities/Publisher.cs
+++ src/Core/Entities/Publisher.cs
@@ -2,7 +2,6 @@ namespace Core.Entities;
 
 public record Publisher() : NamedEntity()
 {
-  public string Name { get; init; }
   public string SiteUrl { get; init; }
 
   public virtual ICollection<Game> Games { get; init; }
