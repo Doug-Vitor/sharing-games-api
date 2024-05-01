2e3cba174e3d551335288528c54bb44c560e810a
Douglas <douglasvitor.dev@gmail.com>
2024-05-01 19:03:21 -0300

    Adding interceptors in dependency injection

--- src/Infra/Configurations/DependencyInjectionConfiguration.cs
+++ src/Infra/Configurations/DependencyInjectionConfiguration.cs
@@ -1,6 +1,8 @@
 using Core.Interfaces;
 using Infra.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -9,14 +11,16 @@ namespace Infra.Configurations;
 public static class DependencyInjectionConfiguration
 {
   public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
-    => services.AddDatabase()
+    => services.AddTransient<IHttpContextAccessor, HttpContextAccessor>()
+               .AddDatabase()
                .AddScoped(typeof(IReadonlyRepository<>), typeof(ReadonlyRepository<>))
                .AddScoped(typeof(IWritableRepository<>), typeof(WritableRepository<>))
-               .AddTransient<AppDbSeeder>();
+               .AddSingleton<AppDbSeeder>();
 
   static IServiceCollection AddDatabase(this IServiceCollection services)
     => services.AddDbContext<AppDbContext>((sp, options) =>
     {
+      options.AddInterceptors(sp.GetServices<IInterceptor>());
       if (sp.GetRequiredService<IHostEnvironment>().IsStaging()) options.UseInMemoryDatabase("InMemoryDatabase");
       else options.UseNpgsql(Environment.GetEnvironmentVariable(Constants.DatabaseConnectionString));
     });
