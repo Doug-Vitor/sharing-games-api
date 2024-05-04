866c7cf38c4fbec444b1c497b8faef90b9a2e13f
Douglas <douglasvitor.dev@gmail.com>
2024-05-04 19:18:33 -0300

    Improving folder structure of core module

---
+++ src/Core/Interfaces/Services/IAuthenticationService.cs
@@ -0,0 +1,13 @@
+using Core.Response;
+using Core.V1.DTOs;
+
+namespace Services.Interfaces;
+
+public interface IAuthenticationService
+{
+  string? AuthenticatedUserId { get; }
+  public Task<ActionResponse> GetAuthenticatedUserAsync();
+  public Task<ActionResponse> SignUpAsync(SignUpInputModel user);
+  public Task<ActionResponse> SignInAsync(SignInInputModel user);
+  public Task<ActionResponse> SignOutAsync();
+}
