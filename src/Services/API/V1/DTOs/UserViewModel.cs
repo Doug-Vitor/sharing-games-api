cd9b9c1b1c249678417ab64a3da359da650fa325
Douglas <douglasvitor.dev@gmail.com>
2024-05-15 18:35:17 -0300

    Adding implicit operator to user view model

--- src/Services/API/V1/DTOs/UserViewModel.cs
+++ src/Services/API/V1/DTOs/UserViewModel.cs
@@ -1,11 +1,16 @@
+using Infra.Entities;
+
 namespace Core.V1.DTOs;
 
 public class UserViewModel()
 {
+  public string Id { get; init; }
   public string UserName { get; init; }
 
   public UserViewModel(string userName) : this() => UserName = userName;
+  public UserViewModel(string id, string userName) : this(userName) => Id = id;
 
+  public static implicit operator UserViewModel(User user) => new(user.Id, user.UserName);
   public static implicit operator UserViewModel(SignInInputModel user) => new(user.UserName);
   public static implicit operator UserViewModel(SignUpInputModel user) => new(user.UserName);
 }
