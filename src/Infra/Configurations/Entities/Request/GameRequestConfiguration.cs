9c0bb0812cbfdc9630719146e188554fdf21cb37
Douglas <douglasvitor.dev@gmail.com>
2024-05-16 19:33:17 -0300

    Adding game relation to game request

--- src/Infra/Configurations/Entities/Request/GameRequestConfiguration.cs
+++ src/Infra/Configurations/Entities/Request/GameRequestConfiguration.cs
@@ -10,6 +10,7 @@ internal class GameRequestConfiguration : IEntityTypeConfiguration<GameRequest>
   public void Configure(EntityTypeBuilder<GameRequest> builder)
   {
     builder.HasOne(typeof(User)).WithMany(nameof(User.GameRequests)).HasForeignKey(nameof(GameRequest.UserId));
+    builder.HasOne(g => g.Game).WithMany().HasForeignKey(g => g.GameId).IsRequired(false);
     builder.Property(g => g.GameUrl).IsRequired();
   }
 }
